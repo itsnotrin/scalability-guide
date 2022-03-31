bdfc878b108f71f922e20ac048a8dfb23a295a3e
itsnotrin <ryanwfun@gmail.com>
2022-03-31 21:34:35 +0100

    get it started

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to understand',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        This guide was designed with you in mind. We kept an open mind whilst making it for it to be as easy as possible for anyone to use!
+      </>
+    ),
+  },
+  {
+    title: 'In depth',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Whilst still being easy to understand, we went into as much detail as we felt was necessary for you to expand upon this knowledge later!
+      </>
+    ),
+  }
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
